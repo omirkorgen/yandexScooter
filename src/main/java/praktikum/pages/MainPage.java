7af532df954b24eddf8d7f82ba88a7462cc6337d
Zhumanali Temirlan <zhumanali2021@gmail.com>
2024-08-24 21:23:34 +0500

    completed the first step of the task

---
+++ src/main/java/praktikum/pages/MainPage.java
@@ -0,0 +1,50 @@
+package praktikum.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import praktikum.EnvConfig;
+
+import java.time.Duration;
+
+import static org.junit.Assert.assertTrue;
+
+public class MainPage {
+    private final WebDriver driver;
+    private final By goButton = By.cssSelector(".Header_Button__28dPO");
+    private final By orderField = By.className("Input_Input__1iN_Z");
+    private final By statusButton = By.className("Header_Link__1TAG7");
+    public WebElement getListItemById(String itemId) {
+        String xpath = ".//div[@id='accordion__heading-" + itemId + "']";
+        return driver.findElement(By.xpath(xpath));
+    };
+    public WebElement getAnswerTextById(String itemId) {
+        String xpath = ".//div[@id='accordion__panel-" + itemId + "']";
+        return driver.findElement(By.xpath(xpath));
+    };
+
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickOnQuestion(String itemId){
+        getListItemById(itemId).click();
+    }
+
+    public String getActualQuestionText(String itemId){
+        return getListItemById(itemId).getText();
+    }
+
+    public String getActualAnswerText(String itemId){
+        return getAnswerTextById(itemId).getText();
+    }
+
+    public void checkAnswerIsVisible(String itemId){
+        new WebDriverWait(driver, Duration.ofSeconds(EnvConfig.EXPLICIT_WAIT))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//div[@id='accordion__heading-" + itemId + "']")));
+        assertTrue(driver.findElement(By.xpath(".//div[@id='accordion__heading-" + itemId + "']")).isDisplayed());
+    }
+
+}
